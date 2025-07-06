bc6e6de0454579def9a975f188874c27116e2d9a
Emory Merryman <emory.merryman@gmail.com>
2025-07-06 20:01:29 +0000

    WTF

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+	inputs = { } ;
+	outputs =
+		{ self } :
+			{
+				lib.implementation =
+					{
+						nixpkgs ,
+						ownertrust ,
+						secret-keys ,
+						system
+					} @primary :
+						let
+							application =	
+								pkgs.writeShellApplication
+									{
+										name = "application" ;
+										runtimeInputs = [ pkgs.coreutils pkgs.gnupg ] ;
+										text =	
+											''
+												export GNUPHOME=/tmp/resources/${ builtins.hashStrings "sha512" ( builtins.toJSON primary ) }
+												mkdir --parents "$GNUPGHOME"
+												chown 07000 "$GNUPGHOME"
+												gpg --homedir "$GNUPGHOME" --batch --yes --import-secret-keys "$( ${ secret-keys } )"
+												gpg --homedir "$GNUPGHOME" --batch --yes --import-ownertrust "( ${ ownertrust } )"
+												gpg --homedir "$GNUPGHOME"" --batch --yes --update-trustdb
+											'' ;
+									} ;
+							pkgs = builtins.getAttr system nixpkgs.legacyPackages ;
+							in
+								"${ application }/bin/application"
+			} ;
+}
