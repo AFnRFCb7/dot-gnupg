bc31d9740770731e6b547d928eb355bbc9243ba9
Emory Merryman <emory.merryman@gmail.com>
2025-11-04 10:58:10 -0500

    dot-gnupg

--- flake.nix
+++ flake.nix
@@ -4,80 +4,81 @@
         { self } :
             {
                 lib =
-                    let
-                        implementation =
-                            { ownertrust-fun , secret-keys-fun } :
-                                {
-                                    init =
-                                        { pkgs , resources , self } :
-                                            let
-                                                application =
-                                                    pkgs.writeShellApplication
-                                                        {
-                                                            name = "init" ;
-                                                            runtimeInputs = [ pkgs.coreutils ] ;
-                                                            text =
-                                                                ''
-                                                                    GNUPGHOME=/mount/dot-gnupg
-                                                                    export GNUPGHOME
-                                                                    mkdir --parents "$GNUPGHOME"
-                                                                    chmod 0700 "$GNUPGHOME"
-                                                                    SECRET_KEYS="${ secret-keys ( setup : setup ) }"
-                                                                    gpg --batch --yes --homedir "$GNUPGHOME" --import "$SECRET_KEYS" 2>&1
-                                                                    OWNERTRUST="${ ownertrust ( setup : setup ) }"
-                                                                    gpg --batch --yes --homedir "$GNUPGHOME" --import-ownertrust "$OWNERTRUST" 2>&1
-                                                                    gpg --batch --yes --homedir "$GNUPGHOME" --update-trustdb 2>&1
-                                                                '' ;
-                                                        } ;
-                                                ownertrust = ownertrust-fun { pkgs = pkgs ; resources = resources ; self = self ; } ;
-                                                secret-keys = secret-keys-fun { pkgs = pkgs ; resources = resources ; self = self ; } ;
-                                                in "${ application }/bin/init" ;
-                                    targets = [ "dot-gnupg" ] ;
-                                } ;
-                            in
-                                {
-                                    check =
-                                        {
-                                            expected ,
-                                            failure ,
-                                            ownertrust-fun ,
-                                            pkgs ,
-                                            resources ? null ,
-                                            secret-keys-fun ,
-                                            self ? null
-                                        } :
-                                            pkgs.stdenv.mkDerivation
-                                                {
-                                                    installPhase =
-                                                        ''
-                                                            execute-test "$out"
-                                                        '' ;
-                                                    name = "check" ;
-                                                    nativeBuildInputs =
-                                                        [
-                                                            (
-                                                                pkgs.writeShellApplication
-                                                                    {
-                                                                        name = "execute-test" ;
-                                                                        runtimeInputs = [ pkgs.coreutils failure ] ;
-                                                                        text =
-                                                                            let
-                                                                                init = instance.init { pkgs = pkgs ; resources = resources ; self = self ; } ;
-                                                                                instance = implementation { ownertrust-fun = ownertrust-fun ; secret-keys-fun = secret-keys-fun ; } ;
-                                                                                in
-                                                                                    ''
-                                                                                        OUT="$1"
-                                                                                        touch "$OUT"
-                                                                                        ${ if [ "init" "targets" ] != builtins.attrNames instance then ''failure 0d792ffc "We expected the dot-gnupg names to be init targets but we observed ${ builtins.toJSON ( builtins.attrNames instance ) }"'' else "#" }
-                                                                                        ${ if expected != init then ''failure b2ba9748 "We expected the dot-gnupg init to be ${ builtins.toString expected } but we observed ${ builtins.toString init }"'' else "#" }
-                                                                                        ${ if [ "dot-gnupg" ] != instance.targets then ''failure 63d0da9f "We expected the dot-gnupg targets to be dot-gnupg but we observed ${ builtins.toJSON instance.targets }"'' else "#" }
-                                                                                    '' ;
-                                                                    }
-                                                            )
-                                                        ] ;
-                                                    src = ./. ;
-                                                } ;
-                                    implementation = implementation ;
-                                } ;
+                    { } :
+                        let
+                            implementation =
+                                { ownertrust-fun , secret-keys-fun } :
+                                    {
+                                        init =
+                                            { pkgs , resources , self } :
+                                                let
+                                                    application =
+                                                        pkgs.writeShellApplication
+                                                            {
+                                                                name = "init" ;
+                                                                runtimeInputs = [ pkgs.coreutils ] ;
+                                                                text =
+                                                                    ''
+                                                                        GNUPGHOME=/mount/dot-gnupg
+                                                                        export GNUPGHOME
+                                                                        mkdir --parents "$GNUPGHOME"
+                                                                        chmod 0700 "$GNUPGHOME"
+                                                                        SECRET_KEYS="${ secret-keys ( setup : setup ) }"
+                                                                        gpg --batch --yes --homedir "$GNUPGHOME" --import "$SECRET_KEYS" 2>&1
+                                                                        OWNERTRUST="${ ownertrust ( setup : setup ) }"
+                                                                        gpg --batch --yes --homedir "$GNUPGHOME" --import-ownertrust "$OWNERTRUST" 2>&1
+                                                                        gpg --batch --yes --homedir "$GNUPGHOME" --update-trustdb 2>&1
+                                                                    '' ;
+                                                            } ;
+                                                    ownertrust = ownertrust-fun { pkgs = pkgs ; resources = resources ; self = self ; } ;
+                                                    secret-keys = secret-keys-fun { pkgs = pkgs ; resources = resources ; self = self ; } ;
+                                                    in "${ application }/bin/init" ;
+                                        targets = [ "dot-gnupg" ] ;
+                                    } ;
+                                in
+                                    {
+                                        check =
+                                            {
+                                                expected ,
+                                                failure ,
+                                                ownertrust-fun ,
+                                                pkgs ,
+                                                resources ? null ,
+                                                secret-keys-fun ,
+                                                self ? null
+                                            } :
+                                                pkgs.stdenv.mkDerivation
+                                                    {
+                                                        installPhase =
+                                                            ''
+                                                                execute-test "$out"
+                                                            '' ;
+                                                        name = "check" ;
+                                                        nativeBuildInputs =
+                                                            [
+                                                                (
+                                                                    pkgs.writeShellApplication
+                                                                        {
+                                                                            name = "execute-test" ;
+                                                                            runtimeInputs = [ pkgs.coreutils failure ] ;
+                                                                            text =
+                                                                                let
+                                                                                    init = instance.init { pkgs = pkgs ; resources = resources ; self = self ; } ;
+                                                                                    instance = implementation { ownertrust-fun = ownertrust-fun ; secret-keys-fun = secret-keys-fun ; } ;
+                                                                                    in
+                                                                                        ''
+                                                                                            OUT="$1"
+                                                                                            touch "$OUT"
+                                                                                            ${ if [ "init" "targets" ] != builtins.attrNames instance then ''failure 0d792ffc "We expected the dot-gnupg names to be init targets but we observed ${ builtins.toJSON ( builtins.attrNames instance ) }"'' else "#" }
+                                                                                            ${ if expected != init then ''failure b2ba9748 "We expected the dot-gnupg init to be ${ builtins.toString expected } but we observed ${ builtins.toString init }"'' else "#" }
+                                                                                            ${ if [ "dot-gnupg" ] != instance.targets then ''failure 63d0da9f "We expected the dot-gnupg targets to be dot-gnupg but we observed ${ builtins.toJSON instance.targets }"'' else "#" }
+                                                                                        '' ;
+                                                                        }
+                                                                )
+                                                            ] ;
+                                                        src = ./. ;
+                                                    } ;
+                                        implementation = implementation ;
+                                    } ;
             } ;
 }
